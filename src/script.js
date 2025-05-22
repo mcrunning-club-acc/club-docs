15ca9323189cdf432c4b614ce52578061dcbbc86
mcrunning-club-acc <mcrunningclub@ssmu.ca>
2025-05-22 17:25:26 -0400

    Update and rename script.gs to script.js

--- src/script.js
+++ src/script.js
@@ -7,3 +7,14 @@
 function multiply(a, b) {
   return a * b;
 }
+
+
+/**
+ * Divides two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function divide(a, b) {
+  return a / b;
+}
